b32c9ac362945df393b96c5a1df054ca577e0867
Alexander Dorn <ad@not.one>
2024-01-10 23:04:08 +0100

    Minor changes

--- keyboards/preonic/keymaps/outergod/keymap.c
+++ keyboards/preonic/keymaps/outergod/keymap.c
@@ -34,28 +34,28 @@ enum preonic_layers {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [DVORAK] = LAYOUT_preonic_1x2uC(
+    [DVORAK] = LAYOUT_preonic_grid(
 KC_GRV,  KC_1,             KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
 KC_TAB,  KC_QUOT,          KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_SLSH,
 KC_LCTL, KC_A,             KC_O,    KC_E,    KC_U,    KC_I,    KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_RCTL,
 KC_LSFT, KC_SCLN,          KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,    KC_RSFT,
-KC_RGUI, TD(TD_ESC_CAPS),  TT(2),   KC_DEL,  KC_LALT,      KC_SPC,      KC_RALT, KC_BSPC, DF(1),   KC_ENT,  KC_RGUI
+KC_RGUI, TD(TD_ESC_CAPS),  KC_MINS, TT(2),   KC_LALT, KC_SPC,  KC_ENT,  KC_RALT, DF(1),   KC_BSPC, KC_DEL,  KC_RGUI
 ),
 
-    [QWERTY] = LAYOUT_preonic_1x2uC(
+    [QWERTY] = LAYOUT_preonic_grid(
 KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 KC_TRNS, KC_Q,             KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_I,    KC_O,    KC_U,    KC_P,    KC_TRNS,
 KC_TRNS, KC_A,             KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_TRNS,
 KC_TRNS, KC_Z,             KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_TRNS,
-KC_TRNS, KC_TRNS,          TT(2),   KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_TRNS, DF(0),   KC_TRNS, KC_TRNS
+KC_TRNS, KC_TRNS,          KC_TRNS, TT(2),   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, DF(0),   KC_TRNS, KC_TRNS, KC_TRNS
 ),
 
-    [FUNCTION] = LAYOUT_preonic_1x2uC(
-KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    [FUNCTION] = LAYOUT_preonic_grid(
+KC_F11,  KC_F1,            KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
 KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_UP,   KC_PGDN, KC_TRNS, KC_EQL,
-KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_RBRC, KC_MINS,
+KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC, KC_LEFT, KC_DOWN, KC_RGHT, KC_RBRC, KC_TRNS,
 KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_NO,   KC_END,  KC_TRNS, KC_TRNS,
-KC_TRNS, KC_LGUI,          KC_TRNS, KC_TRNS, KC_TRNS,      KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+KC_TRNS, KC_LGUI,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 };
 
